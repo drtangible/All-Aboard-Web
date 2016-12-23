f205e6fae03fdc78abd5f8e0845662236366d097
Dan Gilbert <danielcgilbert@gmail.com>
2016-12-23 13:58:29 -0600

    Render search bar.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,37 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      term: '',
+    };
+
+    this._onChange = this._onChange.bind(this);
+  }
+
   render() {
+    let {
+      term,
+    } = this.state;
+
     return (
-      <div>SearchBar</div>
+      <div className="search-bar-container">
+        <input
+          className="search-bar-input"
+          type="text"
+          placeholder="Search for a route"
+          value={term}
+          onChange={this._onChange}
+          />
+      </div>
     );
+  }
+
+  _onChange(e) {
+    this.setState({
+      term: e.target.value,
+    });
   }
 }
